620e18ed5fc1ca349de30175a3f5169525af9dbc
Baboya Choch <BaboyaChoch@Gmail.com>
2020-11-10 00:10:18 -0600

    completed computer actions and began first steps on registering user's answer

--- script.js
+++ script.js
@@ -1,22 +1,63 @@
-let blocks = document.getElementsByClassName("square") //array of square divs, blocks on the board.
-let levelTracker = document.getElementById("levelTracker"); //level counter
-let defaultBlockColor = '#59ceec';
+let defaultBlockColor = '#00ccff';
 
 
-
+//animation for pattern reveal
 function flipAnimation(square) {
     anime({
         targets: square,
-        scale: [{value: 1}, {value: 1.2}, {value: 1, delay: 200}],
-        rotateY: {value: '180', delay: 150},
+        scale: [{value: 1}, {value: 1.3}, {value: 1}],
+        rotateY: {value: '180'},
         backgroundColor: [
             {value: '#ffcc66'},
             {value: '#ff6600'}
           ],
         easing: 'easeInOutSine',
+        duration: 500
+      });   
+}
+
+//reset animation
+function resetBoardAnimation(square) {
+    anime({
+        targets: square,
+        scale: [{value: 1}, {value: 1.3}, {value: 1, delay: 200}],
+        rotateY: {value: '180', delay: 150},
+        backgroundColor: [
+            {value: '#66ffff'},
+            {value: '#00ccff'}
+          ],
+        easing: 'easeInOutSine',
+        duration: 500
+      });   
+}
+
+//animation for pattern reveal
+function correctAnswerAnimation(square) {
+    anime({
+        targets: square,
+        scale: [{value: 1}, {value: 1.2}, {value: 1, delay: 200}],
+        rotateY: {value: '180', delay: 150},
+        backgroundColor: [
+            {value: '#00ff00'},
+            {value: '#ff6600'}
+          ],
+        easing: 'easeInOutSine',
+        duration: 100
+      });   
+}
+
+//animation for wrong answer choice
+function wrongAnswerAnimation(square) {
+    anime({
+        targets: square,
+        scale: [{value: 1}, {value: 1.2}, {value: 1, delay: 200}],
+        rotateY: {value: '180', delay: 150},
+        backgroundColor: '#ff0000',
+        easing: 'easeInOutSine',
         duration: 400
       });   
 }
+
 
 //have a seperate anime for when the user answers
 //have the backgroundColor transition like the above
@@ -38,16 +79,24 @@ function end()
 }
 
 function play(level)
+
 {
+
+    setTimeout( () => gamePattern.forEach((block) => { block.style.background = '#00ccff';}), 500);
+
+    let levelTracker = document.getElementById("levelTracker"); //level counter
+    let blocks = document.getElementsByClassName("square") //array of square divs, blocks on the board.
     let gamePattern = []; //an array of random squares to be memorized
+    
+    levelTracker.innerHTML = `Level:${level}`;
 
-    //based on the level choose {level} number of coordinates and store it in {game_pattern}
+    //based on the level choose a random {level} number of squares/blocks and store it in {game_pattern}
     let current = 0;
-
     choosePattern =  function() {
         if(current < level)
         {
-            let square = document.getElementById(`[${Math.ceil(Math.random()*4-1)},${Math.ceil(Math.random()*4-1)}]`);
+            // change this so that it chooses a random index from 0 to 16 and use blocks[index]
+            let square = blocks[Math.ceil(Math.random()*16-1)]
           
             if(!gamePattern.includes(square)){ 
 
@@ -65,12 +114,11 @@ function play(level)
 
     choosePattern();
   
-    //reveal the pattern to be memorized
+    //reveal the pattern to be memorized to the user
     let index = 0;
 
     revealPattern = setInterval( function() { 
 
-        console.log(index);
         if(index < level)
         {           
             flipAnimation(gamePattern[index]);
@@ -78,34 +126,46 @@ function play(level)
         }
         else{
             clearInterval(revealPattern);
+            //reseting tod default board state
+            gamePattern.forEach((block) => { resetBoardAnimation(block)});
 
-            for(let  i = 0; i < level; i++)
-            {
-                gamePattern[i].style.background = 'red';
-                console.log(gamePattern[i].style.background)
-                console.log(gamePattern[i])
-            }
-
-            //gamePattern.forEach((block) => { block.style.background = 'red';});
-            
-        }
-            
+        }      
        
     }
-    , 600)
+    , (500 + (level)**2))
 
+    let currentIndex = 0;
+    let checkAnswer = function() {
+        let square = document.getElementById(this.getAttribute("id"));
+
+        if(this.getAttribute("id") === gamePattern[currentIndex].getAttribute("id")){
+            correctAnswerAnimation(square);
+            currentIndex++;
+
+            if(currentIndex === level){
+                gamePattern.forEach((block) => { resetBoardAnimation(square)});
+                setTimeout(play(level+1), 1000);
+                
+            }
+                
+        }
+        else{
+           wrongAnswerAnimation(square);
+
+           setTimeout(() => {
+            Array.from(blocks).forEach( (element) => {resetBoardAnimation(element)});
+          }, 100);
+
+          play(1);
+
+        }
+    };
+
+    //listening for click events
+    Array.from(blocks).forEach(function(element) {
+        element.addEventListener('click', checkAnswer);
+      });
   
-
-
-    //listen for click events
-    /*while(current < level )
-    {
-        if($(".square".click(function(){ return this.id})) === random_pattern[current]) 
-            current++;
-        else
-            end();
-    }*/
-
     //ask for the user to input their answer
         //while they answer check to see if they mess up at every step {if id of square they click == game_pattern[i]}
             //if they mess up
@@ -118,12 +178,10 @@ function play(level)
 
 function game()
 {
-    let game_level = 5; //current level of the game ***note: the level is also the number of squares to be flipped"
+    let game_level = 1; //current level of the game ***note: the level is also the number of squares to be flipped"
     let run = true;
-
-    play(game_level);
-
-
+    console.log(play(game_level));
+    
 } 
 
 
